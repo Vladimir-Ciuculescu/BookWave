82a66a9c20aed023ce3f0367a8b5ae71117ac9cf
Vladi <vladimir.ciuculescu@gmail.com>
2024-04-06 20:14:10 +0300

    Added mailtrap package that sends real email

--- server/src/utils/sendEmail.ts
+++ server/src/utils/sendEmail.ts
@@ -1,3 +1,4 @@
+import { MailtrapClient } from "mailtrap";
 import { mailtrap } from "./mailtrap";
 
 interface AttachmentProps {
@@ -15,3 +16,28 @@ export const sendEmail = (to: string, from: string, subject: string, html: strin
     attachments: attachments,
   });
 };
+
+const TOKEN = "33248debbde7bafa1fe327a2440f0107";
+const ENDPOINT = "https://send.api.mailtrap.io/";
+
+const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
+
+const sender = {
+  email: "mailtrap@bookwave.online",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "vladimir.ciuculescu@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
